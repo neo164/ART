f8437e25d9aa04bbcae4b43317f888e78ff941af
Neo164 <predibaylo@gmail.com>
2017-09-04 10:04:26 +0400

    Add files via upload

---
+++ JDBC/src/main/java/neo/testdb/jdbc/DevelopersJdbcDemo.java
@@ -0,0 +1,57 @@
+package neo.testdb.jdbc;
+
+import java.sql.*;
+
+public class DevelopersJdbcDemo {
+    /**
+     * JDBC Driver and database url
+     */
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://neo.myddns.ru:3306/TEST";
+
+    /**
+     * User and Password
+     */
+    static final String USER = "root";
+    static final String PASSWORD = "1234";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        Statement statement = null;
+
+        System.out.println("Registering JDBC driver...");
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        System.out.println("Creating database connection...");
+        connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+
+        System.out.println("Executing statement...");
+        statement = connection.createStatement();
+
+        String sql;
+        sql = "SELECT * FROM developers";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        System.out.println("Retrieving data from database...");
+        System.out.println("\nDevelopers:");
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String specialty = resultSet.getString("specialty");
+            int salary = resultSet.getInt("salary");
+
+            System.out.println("\n================\n");
+            System.out.println("id: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("Specialty: " + specialty);
+            System.out.println("Salary: $" + salary);
+        }
+
+        System.out.println("Closing connection and releasing resources...");
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
